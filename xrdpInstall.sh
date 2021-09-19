7700d4ff27d843295c9e33093dd94890d7c74044
Luciano Soares <soaresluciano@gmail.com>
2021-09-19 20:12:53 +0200

    Update xrdpInstall.sh

--- xrdpInstall.sh
+++ xrdpInstall.sh
@@ -2,11 +2,12 @@
 sudo pacman -Syu --needed base-devel git yay
 yay -S xorgxrdp-git
 
+systemctl enable xrdp.service
+systemctl enable xrdp-sesman.service
+
 sudo cp /etc/X11/Xwrapper.config /etc/X11/Xwrapper.config.original
 echo "allowed_users=anybody" | sudo tee -a /etc/X11/Xwrapper.config
 
 sudo cp ~/.xinitrc ~/.xinitrc.original
+sed -i '$ d' ~/.xinitrc
 echo "dbus-launch startxfce4" | sudo tee -a ~/.xinitrc
-
-systemctl enable xrdp.service
-systemctl enable xrdp-sesman.service
