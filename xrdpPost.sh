b0935a7e8be7a41a1731a7f918cf8d771f3aa411
Luciano Soares <soaresluciano@gmail.com>
2021-09-19 20:57:33 +0200

    Update xrdpPost.sh

--- xrdpPost.sh
+++ xrdpPost.sh
@@ -5,12 +5,12 @@ systemctl enable xrdp-sesman.service
 sudo cp /etc/X11/Xwrapper.config /etc/X11/Xwrapper.config.original
 echo "allowed_users=anybody" | sudo tee -a /etc/X11/Xwrapper.config
 
-sudo cp ~/.xinitrc ~/.xinitrc.original
-sed -i '$ d' ~/.xinitrc
-echo "dbus-launch startxfce4" | sudo tee -a ~/.xinitrc
+#sudo cp ~/.xinitrc ~/.xinitrc.original
+#sed -i '$ d' ~/.xinitrc
+#echo "dbus-launch startxfce4" | sudo tee -a ~/.xinitrc
 
-systemctl start xrdp.service
-systemctl start xrdp-sesman.service
+#systemctl start xrdp.service
+#systemctl start xrdp-sesman.service
 
-systemctl status xrdp
-systemctl status xrdp-sesman.service
+#systemctl status xrdp
+#systemctl status xrdp-sesman.service
