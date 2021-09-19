e66f9deea227361476cb2f251c4735d93479e20f
Luciano Soares <soaresluciano@gmail.com>
2021-09-19 20:22:22 +0200

    Update xrdpInstall.sh

--- xrdpInstall.sh
+++ xrdpInstall.sh
@@ -11,3 +11,9 @@ echo "allowed_users=anybody" | sudo tee -a /etc/X11/Xwrapper.config
 sudo cp ~/.xinitrc ~/.xinitrc.original
 sed -i '$ d' ~/.xinitrc
 echo "dbus-launch startxfce4" | sudo tee -a ~/.xinitrc
+
+systemctl start xrdp.service
+systemctl start xrdp-sesman.service
+
+systemctl status xrdp
+systemctl status xrdp-sesman.service
