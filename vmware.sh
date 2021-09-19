3a2e0d6fa2dcb77e4fb04f92809d098bcb630e7d
Luciano Soares <soaresluciano@gmail.com>
2021-09-19 19:30:12 +0200

    Create vmware.sh

---
+++ vmware.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sudo pacman -Syu --needed open-vm-tools xf86-input-vmmouse xf86-video-vmware mesa gtk2 gtkmm
+
+sudo cp /etc/environment /etc/environment.original
+echo "needs_root_rights=yes" | sudo tee -a /etc/environment
+
+systemctl --now enable vmtoolsd
