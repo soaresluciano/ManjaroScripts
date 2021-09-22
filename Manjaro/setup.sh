5e451b34b74b8444c937ef0c62ed9ca12b3a5d7c
Luciano Soares <soaresluciano@gmail.com>
2021-09-22 20:58:52 +0200

    Update setup.sh

--- Manjaro/setup.sh
+++ Manjaro/setup.sh
@@ -2,6 +2,20 @@
 
 sudo echo "Setup for Manjaro minimal"
 sudo pacman-mirrors --continent 
-sudo pacman -Syyu
+sudo pacman -Syu
 
-sudo pacman -S --needed yay git neofetch htop
+#Xorg and drivers
+sudo pacman -S --needed xorg-server xorg-server-common xorg-xinit
+
+#Network utilities
+sudo pacman -S --needed tigervnc netctl ifplugd iw wpa_supplicant dialog network-manager-applet networkmanager-openvpn
+
+#KDE
+sudo pacman -S --needed plasma-meta
+sudo systemctl enable sddm.service --force
+
+#Utilities
+sudo pacman -S --needed yay git neofetch htop base-devel
+
+#Reboot
+sudo systemctl reboot
