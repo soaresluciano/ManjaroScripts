5eca73fbb99b621d1a45da01b83195415221312c
Luciano Soares <soaresluciano@gmail.com>
2021-09-28 19:07:17 +0200

    Change the manjaro setup script to executable

--- Manjaro/setup.sh
+++ Manjaro/setup.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 source ~/Scripts/cupCake.sh
 
-piOverclock() {
-    Step "Overclock Pi" &&
-    overclockPi /boot/config.txt
-}
-
 updateDistro () {
     Step "Syncronize DB and update system" &&
     sudo pacman-mirrors --fasttrack &&
@@ -15,6 +10,11 @@ updateDistro () {
 installBasics () {
     Step "Install basic utilities" &&
     sudo pacman -S --needed yay neofetch htop base-devel
+}
+
+piOverclock() {
+    Step "Overclock Pi" &&
+    overclockPi /boot/config.txt
 }
 
 Run $0 'askSudo piOverclock updateDistro installBasics'
