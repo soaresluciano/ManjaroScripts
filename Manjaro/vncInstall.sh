e4a9d6cdf4f4076ebd9b7cf8fc2b038a0c3c187a
Luciano Soares <soaresluciano@gmail.com>
2021-10-12 19:28:45 +0200

    Update vncInstall.sh

--- Manjaro/vncInstall.sh
+++ Manjaro/vncInstall.sh
@@ -7,6 +7,20 @@ installPackages (){
     sudo pacman -S --needed tigervnc
 }
 
+sddmUninstall (){
+    Step "Uninstalling SDDM" &&
+    sudo systemctl stop sddm.service &&
+    sudo systemctl disable sddm.service &&
+    sudo pacman -Rs sddm-kcm &&
+}
+
+lightdmInstall (){
+    local file=/etc/lightdm/lightdm.conf
+    Step "Installing lightdm" &&
+    sudo pacman -S lightdm lightdm-slick-greeter &&
+    replaceString $file "#greeter-session=example-gtk-gnome" "greeter-session=lightdm-slick-greeter"
+}
+
 lightdmConfig (){
     local file=/etc/lightdm/lightdm.conf
     Step "Configuring lightdm" &&
@@ -47,4 +61,4 @@ myIp (){
     ip a
 }
 
-Run $0 'askSudo installPackages lightdmConfig gdmConfig socketConfig serviceConfig startService myIp'
+Run $0 'askSudo installPackages sddmUninstall lightdmConfig gdmConfig socketConfig serviceConfig startService myIp'
